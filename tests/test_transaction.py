b229eb29304110b98e5e02d8e14503db0e7df1d2
Joseph Turner <josephturnerjr@gmail.com>
2017-12-10 20:05:37 -0500

    Fix tests

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -1,18 +1,20 @@
 import unittest
 
-from jocoin.tx import Tx, tx_input, tx_output, InvalidTransactionException
+from jocoin.tx import Tx, TxInput, TxOutput, InvalidTransactionException
 import jocoin.crypto as jc
+import jocoin.signature as signature
 
 class TestTx(unittest.TestCase):
     def setUp(self):
         keys = jc.gen_keys(256)
         self.pubkey = keys["pubkey"]
         self.privkey = keys["privkey"]
-        self.inputs = [tx_input(1, 0, 0)]
-        self.outputs = [tx_output("YOU", 5.0), tx_output("ME", 5.0)]
+        self.inputs = [TxInput(1, 0, 0)]
+        self.outputs = [TxOutput("YOU", 5.0), TxOutput("ME", 5.0)]
 
     def test_valid_signature(self):
-        valid = Tx.build_with_signature(self.pubkey, self.privkey, self.inputs, self.outputs)
+        sig = signature.create_signature((self.inputs, self.outputs), self.privkey)
+        valid = Tx(self.pubkey, sig, self.inputs, self.outputs)
         valid.validate()
 
     def test_invalid_signature(self):
